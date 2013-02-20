bcc7bb981237ae0a17d098144914567798068efb
Grant Murphy <gmurphy@redhat.com>
2013-02-20 10:35:45 +1000

    Also display the value of the content being hashed..

--- prototype.py
+++ prototype.py
@@ -8,9 +8,13 @@ for file in sys.argv[1:]:
     f = open(file)
     j = javaclass.JavaClass(f)
     f.close()
+    
+    # This is pretty much just javap -verbose
+    hash_data = str(j)
+    puts(hash_data)
 
     h = hashlib.new("sha1")
-    h.update(str(j))
+    h.update(hash_data)
     puts(columns([h.hexdigest(), 50], [file, 100]))
 
 
